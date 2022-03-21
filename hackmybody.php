ebc8f0bc5cdbd9e2574990ab7c56e559a4bc3752
kana48 <71444352+kana48@users.noreply.github.com>
2022-03-21 21:46:40 +0100

    correct footer

--- hackmybody.php
+++ hackmybody.php
@@ -26,7 +26,6 @@
       <img src="img/hkm3.png" alt="" class="travaux-single-img-horizontal">
   </div>
 </section>
-<a href="?page=travaux" class="travaux-back">Retour à la liste</a>
 <?php 
   include "footer.php"; 
   ?>
