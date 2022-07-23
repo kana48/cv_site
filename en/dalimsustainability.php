e7fc0ed1d11b0ba4c7bee4e649d143cc3b0fc1a9
kana48 <71444352+kana48@users.noreply.github.com>
2022-07-23 09:58:42 +0200

    feature: cookie management and change language sys

---
+++ en/dalimsustainability.php
@@ -0,0 +1,41 @@
+<section class="travaux-single">
+  <div class="intro travaux-single-info">
+    <p>Duration: 2 mois</p>
+    <a href="?page=web">#WEB</a>
+    <p>Coporate Project</p>
+    <hr>
+  </div>
+  <h2 class="travaux-single-title">Dalim sustainability</h2>
+  <p class="travaux-single-subtitle"></p>
+  </section>
+
+  <section>
+  <p class=travaux-single-text>This is my first project as an apprentice at Dalim software, based on a web designer's model, built using Hubspot CMS.
+  </p>
+  <a href="https://sustainability.dalim.com/" class="travaux-single-link">Visit the site</a>
+  <h2>Tools</h2>
+  <p class=travaux-single-text-tool>Hubspot CMS <br class="pc_hid">Figma HTML&CSS JavaScript
+  </p>
+  </section>
+
+<section>
+  <img src="img/dalim-sus-top.png" alt="" class="travaux-single-img-horizontal">
+  <img src="img/dalim-sus-9.png" alt="" class="travaux-single-img-horizontal">
+  <img src="img/dalim-sus-ourapproch.png" alt="" class="travaux-single-img-horizontal">
+  <img src="img/dalim-sus-contact.png" alt="" class="travaux-single-img-horizontal">
+  <!-- <div class="travaux-single-img-vertical">
+    <img src="img/p6.jpg" alt="">
+    <img src="img/p7.jpg" alt="">
+    <img src="img/p8.jpg" alt="">
+  </div> -->
+</section>
+
+
+<section>
+</section>
+
+
+
+<?php 
+  include "footer.php"; 
+  ?>
---
+++ en/mentions-legales.php
@@ -0,0 +1,17 @@
+<section>
+  <h2>Legal notices</h2>
+</section>
+
+<section>
+  <p>Publisher/Host: <br class="pc_hid"> <strong> Kana YAMADA <strong></p>
+  <p>Hosted at OVH,<br class="pc_hid"> 2 Rue Kellermann, 59100 Roubaix.</p>
+</section>
+
+<section>
+  <img src="img/mentions-legales.jpg" alt="" width="375">
+</section>
+
+
+<?php 
+  include "footer.php"; 
+  ?>
